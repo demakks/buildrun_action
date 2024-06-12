96b61bea0988469c82245c7b75eb6b84682627b4
demakks <demakks@tbdir.net>
2024-06-12 14:56:15 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 /**
  * The entrypoint for the action.
  */
-const { run } = require('./main')
+const { Client } = require('pg');
 
-run()
+async function run() {
+  const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'buildrun',
+    password: 'sahana1234',
+    port: 5432,
+  });
+
+  await client.connect();
+
+  const githubRunId = process.env.GITHUB_RUN_ID;
+
+  const query = {
+    text: 'INSERT INTO github_runs(run_id) VALUES($1)',
+    values: [githubRunId],
+  };
+
+  try {
+    await client.query(query);
+    console.log('GitHub run ID stored successfully in PostgreSQL database.');
+  } catch (error) {
+    console.error('Error storing GitHub run ID in PostgreSQL database:', error);
+  } finally {
+    await client.end();
+  }
+}
+
+run();
+
